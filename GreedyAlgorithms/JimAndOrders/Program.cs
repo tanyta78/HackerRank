d665619df095a1287da4c4c35b3b03098c1852a0
Tanya Milanova <tanyta78@gmail.com>
2019-05-13 16:03:07 +0300

    greedy algo - another 2 solutions

---
+++ GreedyAlgorithms/JimAndOrders/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace JimAndOrders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        // Complete the jimOrders function below.
+        static int[] jimOrders(int[][] orders)
+        {
+            var serveTimesPerCustomer = new Dictionary<int,int>();
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                serveTimesPerCustomer.Add(i + 1, orders[i][0] + orders[i][1]);
+            }
+
+            var result = serveTimesPerCustomer.OrderBy(el => el.Value).Select(o=>o.Key).ToArray();
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] orders = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                orders[i] = Array.ConvertAll(Console.ReadLine().Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+            }
+
+            int[] result = jimOrders(orders);
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
---
+++ GreedyAlgorithms/MaxMin/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MaxMin
+{
+    using System;
+
+    class Program
+    {
+        // Complete the maxMin function below.
+        static int maxMin(int k, int[] arr)
+        {
+            Array.Sort(arr);
+            var result = int.MaxValue;
+            var len = arr.Length;
+
+            for (int i = 0; i < len - k + 1; i++)
+            {
+                var curtSubArrDiff = arr[i + k - 1] - arr[i];
+                if (curtSubArrDiff < result)
+                {
+                    result = curtSubArrDiff;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int arrItem = Convert.ToInt32(Console.ReadLine());
+                arr[i] = arrItem;
+            }
+
+            int result = maxMin(k, arr);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
