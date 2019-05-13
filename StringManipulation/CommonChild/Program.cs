18faee14463f5c0899e22facb245ecfd06103d56
Tanya Milanova <tanyta78@gmail.com>
2019-05-13 10:50:16 +0300

    String manipulation - LCS common child

---
+++ StringManipulation/CommonChild/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CommonChild
+{
+    using System;
+
+    class Program
+    {
+        // Complete the commonChild function below.
+        static int commonChild(string s1, string s2) {
+            var s1Length = s1.Length;
+            var s2Length = s2.Length;
+            var memo = new int [s1Length+1,s2Length+1];
+            
+            for (int i = 0; i < s1Length; i++)
+            {
+                for (int j = 0; j < s2Length; j++)
+                {
+                    if (s1[i]==s2[j])
+                    {
+                        memo[i + 1, j + 1] = memo[i, j] + 1;
+                    }
+                    else
+                    {
+                        memo[i + 1, j + 1] = Math.Max(memo[i + 1, j], memo[i, j + 1]);
+                    }
+                }
+            }
+
+            return memo[s1Length, s2Length];
+        }
+
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            int result = commonChild(s1, s2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
--- StringManipulation/SpecialPalindrome/Program.cs
+++ StringManipulation/SpecialPalindrome/Program.cs
@@ -1,9 +1,90 @@
 ﻿namespace SpecialPalindrome
 {
+    using System;
+    using System.Linq;
+
     class Program
     {
+        // Complete the substrCount function below.
+        static long substrCountOpt(int n, string s)
+        {
+            long count = 0;
+            var len = s.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                int innerCounter = 1;
+                int counterDown = 0;
+                int counterUp = 1;
+
+                while (i - innerCounter >= 0 && i + innerCounter < len
+                                             && s[i - innerCounter] == s[i - 1] 
+                                             && s[i + innerCounter] == s[i - 1]) {
+                    count++;
+                    innerCounter++;
+                }
+
+                while (i - counterDown >= 0 && i + counterUp <len 
+                                            && s[i - counterDown] == s[i]
+                                            && s[i + counterUp] == s[i]) {
+                    count++;
+                    counterDown++;
+                    counterUp++;
+                }
+            }
+
+
+            return count + len;
+        }
+
+        static long substrCount(int n, string s)
+        {
+            long palindromeCount = n;
+
+            for (int subLength = 2; subLength <= n; subLength++)
+            {
+                for (int startIndex = 0; startIndex <= n - subLength; startIndex++)
+                {
+                    var subString = s.Skip(startIndex).Take(subLength).ToArray();
+                    if (isPalindrome(subString, subLength))
+                    {
+                        palindromeCount++;
+                    }
+                }
+            }
+
+            return palindromeCount;
+        }
+
+        private static bool isPalindrome(char[] subString, int subLength)
+        {
+            var midLen = subString.Length / 2;
+            var middleChar = subString[midLen];
+
+            if (subString.All(e => e == middleChar))
+            {
+                return true;
+            }
+
+
+            if (subLength % 2 == 0) return false;
+
+            var subList = subString.ToList();
+            subList.RemoveAt(midLen);
+            var lastEl = subList.Last();
+
+            return subList.All(e => e == lastEl);
+        }
+
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            long result = substrCount(n, s);
+
+            Console.WriteLine(result);
         }
     }
 }
